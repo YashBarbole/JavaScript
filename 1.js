e773f6fe79831ef78aa12ed17ca54d1b2139b70c
YashBarbole <yashbarbole7110@gmail.com>
2025-09-06 10:45:47 +0530

    execution context call stack and function

---
+++ 1.js
@@ -0,0 +1,21 @@
+// var n=2;
+// function square(num){
+//     var ans=num*num;
+//     return ans;
+// }
+// var square2= square(n);
+// var square4=square(4);
+
+// var x=1;
+// a();
+// b();
+// console.log(x);
+// function a(){
+//     var x=10;
+//     console.log(x);
+// }
+
+// function b(){
+//     var x=100;
+//     console.log(x);
+// }
